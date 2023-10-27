3aad03ed350c5c73f5c12eb90314b960d82bf447
XavierTho <xavierathompson@gmail.com>
2023-10-27 06:55:30 -0700

    artifact of failed hitbox method

--- assets/js/PlayerHitbox.js
+++ assets/js/PlayerHitbox.js
@@ -1,10 +1,16 @@
-import GameEnv from './GameEnv.js';
-import Character from './Character.js';
 import CharacterPlayer from './Player.js';
 
-export class Hitbox extends CharacterPlayer {
-  constructor(characterPlayer) {
-    this.x = characterPlayer.x;
-    this.y = characterPlayer.y;
-  }
+export class PlayerHitbox extends CharacterPlayer {
+    constructor(player) {
+        super(player.canvas, player.image, player.speedRatio);
+        this.player = player;
+        this.width = 50;
+        this.height = 50;
+    }
+
+    // Function to update the hitbox position based on the player's position
+    update() {
+        this.x = this.player.x + (this.player.width - this.width) / 2;
+        this.y = this.player.y + (this.player.height - this.height) / 2;
+    }
 }
