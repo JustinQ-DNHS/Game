37cf0a82a23afd37fae51129aacc98ee7d0e7464
XavierTho <xavierathompson@gmail.com>
2023-10-31 14:08:37 -0700

    readded getScore()

--- assets/js/Score.js
+++ assets/js/Score.js
@@ -43,4 +43,4 @@ function applyScoreStyles(element) {
 window.addEventListener('load', createScoreDisplay);
 
 //Exports Functions
-export { incrementScore, score };
+export { incrementScore, getScore, score };
